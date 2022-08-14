c2665a0a6e5c02b10b699a6b4fa1a13dd14971c6
Malek-ALdesougi <malekaldesougi@gmail.com>
2022-08-14 17:31:35 +0300

    t

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function getRandomInt(min, max) {
 }
 
 
-let employee1 = new employee(1000, "Ghazi samer", "Administrator", " ", "senior");
-let employee2 = new employee(1001, "Lana Ali", "Finance", " ", "senior");
-let employee3 = new employee(1002, "Tamara Ayoob", "Markiting", " ", "senior");
-let employee4 = new employee(1003, "Safi Walid", "Administrator", " ", "mid-senior");
-let employee5 = new employee(1004, "Omar Zaid", "Developer", " ", "senior");
-let employee6 = new employee(1005, "Rana Saleh", "Administrator", " ", "jonior");
-let employee7 = new employee(1006, "Hadi ahmad", "Finance", " ", "mid-senior");
+let employee1 = new employee(1000, "Ghazi samer", "Administrator",  "senior" , getRandomInt(1000, 2000) );
+let employee2 = new employee(1001, "Lana Ali", "Finance",  "senior", getRandomInt(500, 1000));
+let employee3 = new employee(1002, "Tamara Ayoob", "Markiting",  "senior", getRandomInt(1500, 3000));
+let employee4 = new employee(1003, "Safi Walid", "Administrator",  "mid-senior", getRandomInt(2000, 3000));
+let employee5 = new employee(1004, "Omar Zaid", "Developer",  "senior", getRandomInt(1000, 2000));
+let employee6 = new employee(1005, "Rana Saleh", "Administrator",  "jonior", getRandomInt(1000, 2000));
+let employee7 = new employee(1006, "Hadi ahmad", "Finance",  "mid-senior", getRandomInt(1000, 2300));
